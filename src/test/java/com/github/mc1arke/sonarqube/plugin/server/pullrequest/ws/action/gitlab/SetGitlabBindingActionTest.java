6030eed85f154bf3b1ed4ec2091f7c9527ac7a83
PiekJ <jpiek@quintor.nl>
2020-07-08 15:47:37 +0200

    Bugfix: #201 could not decorate GitLab merge request. Fixed setting the project id on sonarqube project pull decorate option when selecting GitLab. When project id is set use that instead of the sonarqube scanner property. Fixed always evaluating fetching sonarqube scanner property for instance url.

--- src/test/java/com/github/mc1arke/sonarqube/plugin/server/pullrequest/ws/action/gitlab/SetGitlabBindingActionTest.java
+++ src/test/java/com/github/mc1arke/sonarqube/plugin/server/pullrequest/ws/action/gitlab/SetGitlabBindingActionTest.java
@@ -1,17 +1,46 @@
 package com.github.mc1arke.sonarqube.plugin.server.pullrequest.ws.action.gitlab;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-
 import org.junit.Test;
 import org.sonar.api.server.ws.Request;
+import org.sonar.api.server.ws.WebService;
 import org.sonar.db.DbClient;
 import org.sonar.db.alm.setting.ProjectAlmSettingDto;
 import org.sonar.server.component.ComponentFinder;
 import org.sonar.server.user.UserSession;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
 public class SetGitlabBindingActionTest {
+
+    @Test
+    public void testConfigureAction() {
+        DbClient dbClient = mock(DbClient.class);
+        UserSession userSession = mock(UserSession.class);
+        ComponentFinder componentFinder = mock(ComponentFinder.class);
+
+        WebService.NewAction newAction = mock(WebService.NewAction.class);
+
+        WebService.NewParam repositoryParameter = mock(WebService.NewParam.class);
+        when(newAction.createParam(eq("repository"))).thenReturn(repositoryParameter);
+
+        WebService.NewParam almSettingParameter = mock(WebService.NewParam.class);
+        when(almSettingParameter.setRequired(anyBoolean())).thenReturn(almSettingParameter);
+        when(newAction.createParam(eq("almSetting"))).thenReturn(almSettingParameter);
+
+        SetGitlabBindingAction testCase = new SetGitlabBindingAction(dbClient, componentFinder, userSession);
+        testCase.configureAction(newAction);
+
+        verify(newAction).createParam(eq("repository"));
+
+        verify(almSettingParameter).setRequired(eq(true));
+    }
 
     @Test
     public void testCreateProjectAlmSettingDto() {
@@ -20,12 +49,13 @@ public class SetGitlabBindingActionTest {
         ComponentFinder componentFinder = mock(ComponentFinder.class);
 
         Request request = mock(Request.class);
+        when(request.param("repository")).thenReturn("repositoryId");
 
         SetGitlabBindingAction testCase = new SetGitlabBindingAction(dbClient, componentFinder, userSession);
         ProjectAlmSettingDto result = testCase.createProjectAlmSettingDto("projectUuid", "settingsUuid", request);
 
-        assertThat(result).isEqualToComparingFieldByField(new ProjectAlmSettingDto().setProjectUuid("projectUuid").setAlmSettingUuid("settingsUuid"));
+        assertThat(result).isEqualToComparingFieldByField(new ProjectAlmSettingDto().setProjectUuid("projectUuid").setAlmSettingUuid("settingsUuid").setAlmRepo("repositoryId"));
+        verify(request).param("repository");
         verifyNoMoreInteractions(request);
-
     }
 }
