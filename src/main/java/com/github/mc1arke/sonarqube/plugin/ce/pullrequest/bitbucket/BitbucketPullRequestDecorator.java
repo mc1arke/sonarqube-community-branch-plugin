691a7ed196f7b795553341c631b0f5a7a94143d7
Michael Clarke <michael.m.clarke@gmail.com>
2021-07-02 13:49:01 +0100

    Prevent BitBucket unit tests initiating real HTTP calls
    The BitBucket cloud decorator was attempting to perform an OAuth negotiation during construction in unit tests, so was dependent on the availability of BitBucket for the tests to pass. The `BitbucketClientFactory` has been altered to take an OkHttp Client Builder Supplier as an argument, with the tests injecting a mock client builder so no Http calls are performed during the BitBucket decorator tests.

--- src/main/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/bitbucket/BitbucketPullRequestDecorator.java
+++ src/main/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/bitbucket/BitbucketPullRequestDecorator.java
@@ -30,6 +30,7 @@ import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.bitbucket.client.model
 import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.bitbucket.client.model.DataValue;
 import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.bitbucket.client.model.ReportData;
 import com.google.common.annotations.VisibleForTesting;
+import okhttp3.OkHttpClient;
 import org.sonar.api.ce.posttask.QualityGate;
 import org.sonar.api.issue.Issue;
 import org.sonar.api.measures.CoreMetrics;
@@ -67,9 +68,19 @@ public class BitbucketPullRequestDecorator implements PullRequestBuildStatusDeco
             Issue.STATUSES.stream().filter(s -> !Issue.STATUS_CLOSED.equals(s) && !Issue.STATUS_RESOLVED.equals(s))
                     .collect(Collectors.toList());
 
+    private final BitbucketClientFactory bitbucketClientFactory;
+
+    public BitbucketPullRequestDecorator() {
+        this(new BitbucketClientFactory(OkHttpClient.Builder::new));
+    }
+
+    BitbucketPullRequestDecorator(BitbucketClientFactory bitbucketClientFactory) {
+        this.bitbucketClientFactory = bitbucketClientFactory;
+    }
+
     @Override
     public DecorationResult decorateQualityGateStatus(AnalysisDetails analysisDetails, AlmSettingDto almSettingDto, ProjectAlmSettingDto projectAlmSettingDto) {
-        BitbucketClient client = createClient(almSettingDto, projectAlmSettingDto);
+        BitbucketClient client = bitbucketClientFactory.createClient(almSettingDto, projectAlmSettingDto);
         try {
             if (!client.supportsCodeInsights()) {
                 LOGGER.warn("Your Bitbucket instance does not support the Code Insights API.");
@@ -97,11 +108,6 @@ public class BitbucketPullRequestDecorator implements PullRequestBuildStatusDeco
         }
 
         return DEFAULT_DECORATION_RESULT;
-    }
-
-    @VisibleForTesting
-    BitbucketClient createClient(AlmSettingDto almSettingDto, ProjectAlmSettingDto projectAlmSettingDto) {
-        return BitbucketClientFactory.createClient(almSettingDto, projectAlmSettingDto);
     }
 
     @Override
--- src/test/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/bitbucket/client/BitbucketClientFactoryUnitTest.java
+++ src/test/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/bitbucket/client/BitbucketClientFactoryUnitTest.java
@@ -1,35 +1,49 @@
 package com.github.mc1arke.sonarqube.plugin.ce.pullrequest.bitbucket.client;
 
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.sonar.db.alm.setting.ALM;
 import org.sonar.db.alm.setting.AlmSettingDto;
 import org.sonar.db.alm.setting.ProjectAlmSettingDto;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BitbucketClientFactoryUnitTest {
 
     @Test
-    public void testCreateClientIsCloudIfCloudConfig() {
+    public void testCreateClientIsCloudIfCloudConfig() throws IOException {
         // given
         AlmSettingDto almSettingDto = new AlmSettingDto().setAlm(ALM.BITBUCKET_CLOUD);
         ProjectAlmSettingDto projectAlmSettingDto = new ProjectAlmSettingDto();
+        OkHttpClient.Builder builder = mock(OkHttpClient.Builder.class, Mockito.RETURNS_DEEP_STUBS);
+        when(builder.addInterceptor(any())).thenReturn(builder);
+
+        ResponseBody responseBody = mock(ResponseBody.class);
+        when(responseBody.string()).thenReturn("{}");
+        when(builder.build().newCall(any()).execute().body()).thenReturn(responseBody);
 
         // when
-        BitbucketClient client = BitbucketClientFactory.createClient(almSettingDto, projectAlmSettingDto);
+        BitbucketClient client = new BitbucketClientFactory(() -> builder).createClient(almSettingDto, projectAlmSettingDto);
 
         // then
         assertTrue(client instanceof BitbucketCloudClient);
     }
 
     @Test
-    public void testCreateClientIfNotCLoudConfig() {
+    public void testCreateClientIfNotCloudConfig() {
         // given
         AlmSettingDto almSettingDto = new AlmSettingDto().setAlm(ALM.BITBUCKET);
         ProjectAlmSettingDto projectAlmSettingDto = new ProjectAlmSettingDto();
 
         // when
-        BitbucketClient client = BitbucketClientFactory.createClient(almSettingDto, projectAlmSettingDto);
+        BitbucketClient client = new BitbucketClientFactory(() -> mock(OkHttpClient.Builder.class, Mockito.RETURNS_DEEP_STUBS)).createClient(almSettingDto, projectAlmSettingDto);
 
         // then
         assertTrue(client instanceof BitbucketServerClient);
