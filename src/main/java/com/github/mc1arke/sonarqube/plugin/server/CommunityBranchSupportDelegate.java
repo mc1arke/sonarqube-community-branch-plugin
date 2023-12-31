b034d100caf1423aeb5f4d8e65d8822e86ecaf1e
Michael Clarke <michael.m.clarke@gmail.com>
2023-12-31 12:29:52 +0000

    #829: Add support for Sonarqube 10.3
    The constants in `CeTaskCharacteristicsDto` have been moved into a new
    class `CeTaskCharacteristics`, so the references in the plugin have
    been updated.

--- src/main/java/com/github/mc1arke/sonarqube/plugin/server/CommunityBranchSupportDelegate.java
+++ src/main/java/com/github/mc1arke/sonarqube/plugin/server/CommunityBranchSupportDelegate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2023 Michael Clarke
+ * Copyright (C) 2020-2024 Michael Clarke
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -26,11 +26,11 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.sonar.api.config.Configuration;
+import org.sonar.core.ce.CeTaskCharacteristics;
 import org.sonar.core.config.PurgeConstants;
 import org.sonar.core.util.UuidFactory;
 import org.sonar.db.DbClient;
 import org.sonar.db.DbSession;
-import org.sonar.db.ce.CeTaskCharacteristicDto;
 import org.sonar.db.component.BranchDto;
 import org.sonar.db.component.BranchType;
 import org.sonar.db.component.ComponentDto;
@@ -59,20 +59,20 @@ public class CommunityBranchSupportDelegate implements BranchSupportDelegate {
 
     @Override
     public CommunityComponentKey createComponentKey(String projectKey, Map<String, String> characteristics) {
-        String branchTypeParam = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristicDto.BRANCH_TYPE_KEY));
+        String branchTypeParam = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristics.BRANCH_TYPE));
 
         if (null == branchTypeParam) {
-            String pullRequest = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristicDto.PULL_REQUEST));
+            String pullRequest = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristics.PULL_REQUEST));
             if (null == pullRequest) {
                 throw new IllegalArgumentException(String.format("One of '%s' or '%s' parameters must be specified",
-                                                                 CeTaskCharacteristicDto.BRANCH_TYPE_KEY,
-                                                                 CeTaskCharacteristicDto.PULL_REQUEST));
+                                                                 CeTaskCharacteristics.BRANCH_TYPE,
+                                                                 CeTaskCharacteristics.PULL_REQUEST));
             } else {
                 return new CommunityComponentKey(projectKey, null, pullRequest);
             }
         }
 
-        String branch = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristicDto.BRANCH_KEY));
+        String branch = StringUtils.trimToNull(characteristics.get(CeTaskCharacteristics.BRANCH));
 
         try {
             BranchType.valueOf(branchTypeParam);
