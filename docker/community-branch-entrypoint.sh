d832fa15d73dc5f8b60f941b72741f892adbdb0d
Nacho-Larreta <nachitolarreta@gmail.com>
2026-08-31 16:17:26 -0300

    Fix plugin upgrades for release images

--- docker/community-branch-entrypoint.sh
+++ docker/community-branch-entrypoint.sh
@@ -3,14 +3,17 @@
 set -eu
 
 plugin_name="sonarqube-community-branch-plugin-${PLUGIN_VERSION}.jar"
-plugin_source="/opt/sonarqube/lib/community-branch-plugin/${plugin_name}"
-plugin_directory="/opt/sonarqube/extensions/plugins"
-disabled_directory="/opt/sonarqube/extensions/disabled-plugins"
+sonarqube_home="${SONARQUBE_HOME:-/opt/sonarqube}"
+plugin_source="${sonarqube_home}/lib/community-branch-plugin/${plugin_name}"
+plugin_directory="${sonarqube_home}/extensions/plugins"
+disabled_directory="${sonarqube_home}/extensions/disabled-plugins"
 plugin_target="${plugin_directory}/${plugin_name}"
 
 mkdir -p "${plugin_directory}" "${disabled_directory}"
 
-for installed_plugin in "${plugin_directory}"/sonarqube-community-branch-plugin-*.jar; do
+for installed_plugin in \
+  "${plugin_directory}/sonarqube-community-branch-plugin.jar" \
+  "${plugin_directory}"/sonarqube-community-branch-plugin-*.jar; do
   [ -f "${installed_plugin}" ] || continue
   [ "${installed_plugin}" = "${plugin_target}" ] ||
     mv "${installed_plugin}" "${disabled_directory}/$(basename "${installed_plugin}").disabled"
@@ -21,4 +24,4 @@ cp "${plugin_source}" "${plugin_temporary}"
 chmod 644 "${plugin_temporary}"
 mv "${plugin_temporary}" "${plugin_target}"
 
-exec /opt/sonarqube/docker/entrypoint.sh "$@"
+exec "${sonarqube_home}/docker/entrypoint.sh" "$@"
