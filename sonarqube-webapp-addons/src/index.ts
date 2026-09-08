a68cf75cc414a0725e54ec73d7a1ff6d51afadd7
Adrian Eckardt <adrian@MacBookPro.bb.online.no>
2026-09-08 16:37:22 +0200

    Fix SonarQube 26.9 entitlement addons

--- sonarqube-webapp-addons/src/index.ts
+++ sonarqube-webapp-addons/src/index.ts
@@ -1,3 +1,4 @@
 import branches from './branches';
+import * as entitlements from './feature-license/entitlements';
 
-export const addons = { branches };
+export const addons = { branches, entitlements };
